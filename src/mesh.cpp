0121b32776dc05883207ca45f5a86eebf18311dd
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2026-07-15 00:57:40 +0200

    perf(mesh): move VBO, VAO to init phase

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,13 +1,9 @@
 #include "mesh.hpp"
 #include "gl.hpp"
-#include "vao.hpp"
 
 namespace goon {
 
 auto Mesh::draw() const -> void {
-    const auto vbo{VBO{this->vertices}};
-    const auto vao{VAO{vbo}};
-
     glDrawArrays(
         GL_TRIANGLE_STRIP, 0, static_cast<int32_t>(this->vertices.size())
     );
--- src/mesh.hpp
+++ src/mesh.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
-#include "point.hpp"
-#include <span>
+#include "vao.hpp"
 #include <vector>
 
 namespace goon {
 
 struct Mesh final {
     std::vector<Point> vertices;
+    const VBO vbo{this->vertices};
+    const VAO vao{this->vbo};
 
     explicit constexpr Mesh(std::span<const Point> vertices)
         : vertices{vertices.begin(), vertices.end()} {}
